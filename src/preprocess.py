057c9b43b28c4261cd67ca141d3ae6b5744f73b9
eugeniaring <eugenia.anello@gmail.com>
2023-03-04 01:17:57 +0100

    add

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,18 +1,9 @@
 import pandas as pd
-from sklearn.model_selection  import train_test_split
-from config.config_path import PATHS,cb_features
+from config.config_path import PATHS
 
 def preprocessing(df):
     df = df.drop(['instant','dteday','atemp','casual','registered'],axis=1)
     return df
-
-def split(df):
-    X,y = df[cb_features['feature_names']],df[cb_features['target'][0]]
-    return train_test_split(X,y,test_size=0.2,random_state=123) 
-
-def change_dtype(df):
-    for c in ['season','yr','mnth','hr','holiday','weekday','workingday','weathersit']:
-        df[c] = df[c].astype('object')
 
 if __name__ == "__main__":
     df = pd.read_csv(PATHS['raw_data'],delimiter=',')
--- src/train.py
+++ src/train.py
@@ -3,17 +3,25 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from config.config_path import PATHS,cb_features
 from preprocess import change_dtype
-from sklearn.model_selection import train_test_split
 from catboost import CatBoostRegressor
 import pickle
 from preprocess import split
+from dvc.api import params_show
+
+def split(df,test_size):
+    X,y = df[cb_features['feature_names']],df[cb_features['target'][0]]
+    return train_test_split(X,y,test_size=test_size,random_state=123) 
+
+def change_dtype(df):
+    for c in ['season','yr','mnth','hr','holiday','weekday','workingday','weathersit']:
+        df[c] = df[c].astype('object')
 
 if __name__ == "__main__":
     df = pd.read_parquet(PATHS['preprocessed_data'])
     change_dtype(df)
     print(df.info())
     #print(list(df.columns))
-    X_train, X_test, y_train, y_test = split(df)
+    X_train, X_test, y_train, y_test = split(df,test_size=params_show()['split'])
     categorical_indices = np.where(df.dtypes=='object')[0]
     categorical_indices = categorical_indices.tolist()
     print(categorical_indices)
